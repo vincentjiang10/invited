1fc6c58b90981c770b8ea30a41905f3ff3f1d7dc
SohitGuru <sohitgrng@gmail.com>
2023-05-05 14:29:40 -0400

    Filtering for public events and created events done. Invited events will filter to empty events because event invitation not set up yet.

--- frontend/FinalProj/FinalProj/ViewController.swift
+++ frontend/FinalProj/FinalProj/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     let tableView = UITableView()
     var eventData: [Event] = []
     var filteredEvents: [Event] = []
+    var createdEvents: [Event] = []
+    var invitedEvents: [Event] = []
     var filterActive = false
+    var activeFilterName: String?
     
     var filterCollectionView: UICollectionView!
 
@@ -24,7 +27,6 @@ class ViewController: UIViewController {
     let refreshControl = UIRefreshControl()
     
     var filters: [String] = ["Public", "Invited", "Created"]
-    var newfilters: [String] = []
     
     let itemPadding: CGFloat = 10
     let distPadding: CGFloat = 50
@@ -117,32 +119,21 @@ class ViewController: UIViewController {
                 self.tableView.reloadData()
             }
         }
-        
     }
-    
-    func filter(filter: String) {
+    func filter() {
         if filterActive {
-            if filter == "Public" {
-                for event in eventData {
-                    if event.access == "Public" {
-                        filteredEvents.append(event)
-                    }
-                }
-            } else if filter == "Invited" {
-                for event in eventData {
-                    if event.name == "Slug Club" {
-                        filteredEvents.append(event)
-                    }
-                }
-            } else if filter == "Created" {
-                for event in eventData {
-                    if event.name == "Slug Club" {
-                        filteredEvents.append(event)
-                    }
-                }
+            if activeFilterName == "Public" {
+                refreshData()
+                filteredEvents = eventData
+                eventData = filteredEvents
+            } else if activeFilterName == "Invited" {
+                invitedEvents = []
+                eventData = invitedEvents
+            } else if activeFilterName == "Created" {
+                eventData = createdEvents
             }
         } else {
-            filteredEvents = eventData
+            refreshData()
         }
     }
 
@@ -167,6 +158,7 @@ class ViewController: UIViewController {
     }
     
 }
+
 extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
@@ -194,34 +186,54 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
         return UICollectionViewCell()
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? FilterCollectionViewCell{
+        if let cell = collectionView.cellForItem(at: indexPath) as? FilterCollectionViewCell {
+            let newtext = cell.label.text!
             if filterActive {
-                cell.isSelected = false
-                filterActive = false
-                cell.label.backgroundColor = .systemMint
+                if activeFilterName == newtext {
+                    cell.isSelected = false
+                    activeFilterName = nil
+                    filterActive = false
+                    filter()
+                    cell.label.backgroundColor = .systemMint
+                    tableView.reloadData()
+                } else {
+                    let activeIndexPath = IndexPath(item: filters.firstIndex(of: activeFilterName!)!, section: 0)
+                    if let activeCell = collectionView.cellForItem(at: activeIndexPath) as? FilterCollectionViewCell {
+                        activeCell.isSelected = false
+                        activeFilterName = newtext
+                        filter()
+                        activeCell.label.backgroundColor = .systemMint
+                        cell.isSelected = true
+                        cell.label.backgroundColor = .systemCyan
+                        tableView.reloadData()
+                    }
+                }
             } else {
                 cell.isSelected = true
+                activeFilterName = newtext
                 filterActive = true
+                filter()
                 cell.label.backgroundColor = .systemCyan
+                tableView.reloadData()
             }
-            
         }
     }
+
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? FilterCollectionViewCell{
+        if let cell = collectionView.cellForItem(at: indexPath) as? FilterCollectionViewCell {
             cell.isSelected = false
             filterActive = false
+            activeFilterName = nil
+            filter()
+            tableView.reloadData()
             cell.label.backgroundColor = .systemMint
-            
         }
     }
-    
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = (view.frame.width / 3.2)
         return CGSize(width: width, height: 50)
         }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: 0, height: 0)
     }
@@ -231,58 +243,85 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.currentIndex = indexPath
-        let currentEvent = eventData[indexPath.row]
-        
-        let vc = EventDetailViewController(event: currentEvent)
-        
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->
-    Int {
         if filterActive {
-            // filteredEvents.count
-            return eventData.count
-        } else {
-            return eventData.count
+            if activeFilterName == "Public" {
+                let currentEvent = filteredEvents[indexPath.row]
+                let vc = EventDetailViewController(event: currentEvent)
+                navigationController?.pushViewController(vc, animated: true)
+            } else if activeFilterName == "Invited" {
+                print("No invites")
+            } else if activeFilterName == "Created" {
+                let currentEvent = createdEvents[indexPath.row]
+                let vc = EventDetailViewController(event: currentEvent)
+                navigationController?.pushViewController(vc, animated: true)
+                
+            } else {
+                let currentEvent = eventData[indexPath.row]
+                
+                let vc = EventDetailViewController(event: currentEvent)
+                
+                navigationController?.pushViewController(vc, animated: true)
+            }
         }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath) as! EventTableViewCell
-        if filterActive {
-            // filteredEvents[indexPath.row]
-            let currentEvent = eventData[indexPath.row]
-            cell.configure(eventObject: currentEvent)
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->
+        Int {
+            if filterActive {
+                if activeFilterName == "Public" {
+                    return filteredEvents.count
+                } else if activeFilterName == "Invited" {
+                    return 0
+                } else if activeFilterName == "Created" {
+                    return createdEvents.count
+                }
+                
+            }
+            return eventData.count
         }
-        else {
-            let currentEvent = eventData[indexPath.row]
-            cell.configure(eventObject: currentEvent)
-        }
-        let topBorder = CALayer()
-        topBorder.frame = CGRect(x: 0, y: 0, width: cell.contentView.frame.width, height: 0.5)
-        topBorder.backgroundColor = UIColor.purple.cgColor
-        cell.contentView.layer.addSublayer(topBorder)
         
-        let bottomBorder = CALayer()
-        bottomBorder.frame = CGRect(x: 0, y: cell.contentView.frame.height, width: cell.contentView.frame.width, height: 0.5)
-        bottomBorder.backgroundColor = UIColor.purple.cgColor
-        cell.contentView.layer.addSublayer(bottomBorder)
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath) as! EventTableViewCell
+            if filterActive {
+                if activeFilterName == "Public" {
+                    let currentEvent = filteredEvents[indexPath.row]
+                    cell.configure(eventObject: currentEvent)
+                } else if activeFilterName == "Invited" {
+                    let currentEvent = invitedEvents[indexPath.row]
+                    cell.configure(eventObject: currentEvent)
+                } else if activeFilterName == "Created" {
+                    let currentEvent = createdEvents[indexPath.row]
+                    cell.configure(eventObject: currentEvent)
+                }
+            }
+            else {
+                let currentEvent = eventData[indexPath.row]
+                cell.configure(eventObject: currentEvent)
+            }
+            let topBorder = CALayer()
+            topBorder.frame = CGRect(x: 0, y: 0, width: cell.contentView.frame.width, height: 0.5)
+            topBorder.backgroundColor = UIColor.purple.cgColor
+            cell.contentView.layer.addSublayer(topBorder)
             
-        return cell
-        
+            let bottomBorder = CALayer()
+            bottomBorder.frame = CGRect(x: 0, y: cell.contentView.frame.height, width: cell.contentView.frame.width, height: 0.5)
+            bottomBorder.backgroundColor = UIColor.purple.cgColor
+            cell.contentView.layer.addSublayer(bottomBorder)
+            
+            return cell
+            
         }
-    
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return false
+        
+        
+        func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+            return false
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 112
+        }
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 112
-    }    
-}
-
 extension ViewController: ChangeTextDelegate {
     func changeText(nametext: String, emailtext: String) {
         title = nametext + "'s Event Feed"
@@ -295,5 +334,6 @@ extension ViewController: MakeEventDelegate {
         NetworkManager.shared.createEvent(nametext: nametext, starttime: starttime, endtime: endtime, loc: loc, acc: acc, descrip: descrip) { event in
             print("success")
         }
+        createdEvents.append(Event(id: 0, name: nametext, start_time: starttime, end_time: endtime, location: loc, access: acc, description: descrip))
     }
 }
